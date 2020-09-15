9bdcd60447e0f87e3117cf14b4c0cf9145512ca1
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-15 10:35:24 -0600

    Set up to use django built in login and log out view.
    created dashboard for  after successful login

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 def user_login(request):
     form = LoginForm(request.POST)
@@ -21,4 +22,6 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
