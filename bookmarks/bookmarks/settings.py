b481805911e9cbcedeedc4125a44d4ce17036641
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-17 11:55:50 -0600

    Added social authentication
    twitter, facebook, google
    
    	modified:   bookmarks/account/templates/registration/login.html
    	modified:   bookmarks/bookmarks/__pycache__/settings.cpython-38.pyc
    	modified:   bookmarks/db.sqlite3

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -134,4 +134,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
 ]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '1021982588250255' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '403547f2ed40da37f7cc19ea48fb9e05' # Facebook App Secret
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_TWITTER_KEY = 't5REnL9vykYF0ABJ1IskrM2uj' # Twitter API Key
+SOCIAL_AUTH_TWITTER_SECRET = 'rgIXjQBDVSiZpdclYxNH5myjw1z12782Dqo7T2pOtyavJptEB2' # Twitter API Secret
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '666473336514-85dh68bo5houla8m2tvhf7cennplrqs9.apps.googleusercontent.com' # Google Consumer Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'E0QJ7Bpe7bYDDIwAQ-vvSTVk' # Googole Consumer Secret
