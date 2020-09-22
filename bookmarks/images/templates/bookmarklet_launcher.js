7636950d9eb71add40055c87bd5370c2d9ef462c
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-22 10:57:33 -0600

    missed port and closing ()

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -1,8 +1,8 @@
 (function(){
-    if (window.myBookmarlet !== undefined) {
-        myBookmarlet();
+    if (window.myBookmarklet !== undefined){
+        myBookmarklet();
     }
-    else{
-        document.body.appendChild(document.createElement('script')).src='https://127.0.0.1/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999)
+    else {
+        document.body.appendChild(document.createElement('script')).src='https://127.0.0.1:8000/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
     }
-});
+})();
